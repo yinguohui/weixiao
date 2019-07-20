4bdb497993b0b099ec03f4218cbd73bfe6d9993d
ganjie <ganjie@xylink.com>
2019-07-20 15:14:34 +0800

    lombok插件

--- src/main/java/com/xihua/weixiao/entiy/People.java
+++ src/main/java/com/xihua/weixiao/entiy/People.java
@@ -1,5 +1,6 @@
 package com.xihua.weixiao.entiy;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,32 +11,8 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 @ConfigurationProperties(prefix = "my1")
+@Data
 public class People {
     private String name;
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "People{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
 }
