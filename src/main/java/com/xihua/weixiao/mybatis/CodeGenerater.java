947b2f2b79a30e91a593ae6b0ad14b5f41b9e8d2
ganjie <ganjie@xylink.com>
2019-07-21 20:51:26 +0800

    代码生成

---
+++ src/main/java/com/xihua/weixiao/mybatis/CodeGenerater.java
@@ -0,0 +1,85 @@
+package com.xihua.weixiao.mybatis;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * @author ：ganjie
+ * @description：TODO
+ * @date ：Created in 2019-07-21 20:05
+ * @modified By：
+ */
+public class CodeGenerater {
+
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir("/Users/ganjie/workspace/weixiao/src/main/java/com/xihua/weixiao");
+        gc.setFileOverride(true);
+        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor("GanJie");// 作者
+
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        mpg.setGlobalConfig(gc);
+
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("ganjie");
+        dsc.setPassword("GJasdfgy7345672");
+        dsc.setUrl("jdbc:mysql://47.112.28.150:3306/weixiao?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allo");
+        mpg.setDataSource(dsc);
+
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix(new String[]{""});// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[]{"user"}); // 需要生成的表
+
+
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+
+
+        mpg.setStrategy(strategy);
+
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.xihua.weixiao");
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("serviceImpl");
+        pc.setMapper("mapper");
+        pc.setEntity("entity");
+        pc.setXml("xml");
+        mpg.setPackageInfo(pc);
+
+        // 执行生成
+        mpg.execute();
+
+
+    }
+
+}
