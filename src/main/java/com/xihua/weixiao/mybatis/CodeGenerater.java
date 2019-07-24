2f74652afce7dc816929d2d2cd851bc5b31183c7
yinguohui <15196622412@163.com>
2019-07-24 19:57:40 +0800

    first:提交映射文件

--- src/main/java/com/xihua/weixiao/mybatis/CodeGenerater.java
+++ src/main/java/com/xihua/weixiao/mybatis/CodeGenerater.java
@@ -22,13 +22,13 @@ public class CodeGenerater {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("/Users/ganjie/workspace/weixiao/src/main/java/com/xihua/weixiao");
+        gc.setOutputDir("D:\\C_Code\\G_GIT\\Git\\weixiao\\src\\main\\java\\com\\xihua\\weixiao");
         gc.setFileOverride(true);
         gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
         gc.setEnableCache(false);// XML 二级缓存
         gc.setBaseResultMap(true);// XML ResultMap
         gc.setBaseColumnList(false);// XML columList
-        gc.setAuthor("GanJie");// 作者
+        gc.setAuthor("YGH");// 作者
 
 
         // 自定义文件命名，注意 %s 会自动填充表实体属性！
@@ -44,8 +44,8 @@ public class CodeGenerater {
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setDbType(DbType.MYSQL);
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
-        dsc.setUsername("ganjie");
-        dsc.setPassword("GJasdfgy7345672");
+        dsc.setUsername("yinguohui");
+        dsc.setPassword("Haohaozhenxi5@");
         dsc.setUrl("jdbc:mysql://47.112.28.150:3306/weixiao?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allo");
         mpg.setDataSource(dsc);
 
@@ -54,8 +54,9 @@ public class CodeGenerater {
         StrategyConfig strategy = new StrategyConfig();
         strategy.setTablePrefix(new String[]{""});// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        strategy.setInclude(new String[]{"user"}); // 需要生成的表
-
+        strategy.setInclude(new String[]{"user","chat","comment_detail"
+                ,"donation","goods","goods_type","like_detail","lostinfo",
+                "message","place","review","suggestion","topic"}); // 需要生成的表
 
         strategy.setSuperServiceClass(null);
         strategy.setSuperServiceImplClass(null);
